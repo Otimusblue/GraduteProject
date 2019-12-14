95c168df8787ce1fdf3fc14c1e25514d6dbd4d30
Hieu <otimusblue@gmail.com>
2019-12-14 19:58:44 +0700

    FInal

--- graduatedproject/app/src/main/java/com/example/graduatedproject/FeedActivity.java
+++ graduatedproject/app/src/main/java/com/example/graduatedproject/FeedActivity.java
@@ -1,13 +1,64 @@
 package com.example.graduatedproject;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FeedActivity extends AppCompatActivity {
 
+    DatabaseReference rootRef;
+    Button btnFeed,rsFeed;
+    TextView tvFeed;
+    String set_feed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_feed);
+
+        btnFeed = findViewById(R.id.feed_button);
+        rsFeed = findViewById(R.id.reset_button);
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        tvFeed = findViewById(R.id.tv_feed);
+
+        btnFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                                        rootRef.child("SERVO").setValue("ON");
+                                        tvFeed.setText("Your fishes are full.");
+                                        btnFeed.setEnabled(false);
+                                        Toast.makeText(FeedActivity.this, "Feed the fishs!",
+                                                Toast.LENGTH_LONG).show();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+            }
+        });
+        rsFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    rootRef.child("SERVO").setValue("OFF");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tvFeed.setText("Your fishes are hungry now. Do you want to feed ?");
+        btnFeed.setEnabled(true);
+        rootRef.child("SERVO").setValue("OFF");
     }
 }
--- graduatedproject/app/src/main/java/com/example/graduatedproject/TimingActivity.java
+++ graduatedproject/app/src/main/java/com/example/graduatedproject/TimingActivity.java
@@ -1,19 +1,34 @@
 package com.example.graduatedproject;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.Calendar;
 
-public class TimingActivity extends AppCompatActivity {
+public class TimingActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText day_edt, light_on_edt, light_off_edt, feed_on_edt;
     Button day_btn, set_light_on_btn,set_light_off_btn, submit_light_btn, set_feed_btn,submit_feed_btn;
+    private int mYear,mMonth,mDay,mHour,mMinute;
+    String setTimeToDatabase;
+    DatabaseReference rootRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timing);
-
+        rootRef = FirebaseDatabase.getInstance().getReference();
         day_edt = findViewById(R.id.day_edt);
         light_on_edt = findViewById(R.id.set_on_edt);
         light_off_edt = findViewById(R.id.set_off_edt);
@@ -26,7 +41,76 @@ public class TimingActivity extends AppCompatActivity {
         set_feed_btn = findViewById(R.id.set_feed_button);
         submit_feed_btn = findViewById(R.id.submit_feed_button);
 
-        
+        day_btn.setOnClickListener(this);
+        set_light_on_btn.setOnClickListener(this);
+        set_light_off_btn.setOnClickListener(this);
+        set_feed_btn.setOnClickListener(this);
+        submit_feed_btn.setOnClickListener(this);
+        submit_light_btn.setOnClickListener(this);
+
+
+
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == day_btn){
+            final Calendar calendar = Calendar.getInstance();
+            mYear = calendar.get(Calendar.YEAR);
+            mMonth = calendar.get(Calendar.MONTH);
+            mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    setTimeToDatabase = dayOfMonth+"-"+(month+1)+"-"+year;
+                    day_edt.setText(dayOfMonth+"-"+(month+1)+"-"+year);
+
+
+                }
+            },mYear,mMonth,mDay);
+            datePickerDialog.show();
+        }
+        if (v == set_feed_btn){
+            final Calendar calendar = Calendar.getInstance();
+            mHour = calendar.get(Calendar.HOUR);
+            mMinute = calendar.get(Calendar.MINUTE);
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    feed_on_edt.setText(hourOfDay+":"+ minute);
+                }
+            },mHour,mMinute,true);
+            timePickerDialog.show();
+
+
+
+            rootRef.child("SETTIME").setValue(setTimeToDatabase+""+mHour+mMinute);
+            rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    new android.os.Handler().postDelayed(new Runnable() {
+                                                             @Override
+                                                             public void run() {
+                                                                 rootRef.child("SETTIME").setValue("THANH CONG");
+                                                             }
+                                                         },
+                            30*1000);
+                }
+
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+    }
+
+
+
 }
